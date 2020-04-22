wrong result

main.py
@@ -1,10 +1,5 @@
 import sys
 import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
-import numpy as np
-import datetime as dt
-from PIL import Image
 import numpy as np
 import cv2
 
@@ -16,103 +11,71 @@ def load_pb(path_to_pb):
         tf.import_graph_def(graph_def, name='')
         return graph
 
-def convert_to_opencv(image):
-    # RGB -> BGR conversion is performed as well.
-    image = image.convert('RGB')
-    r,g,b = np.array(image).T
-    opencv_image = np.array([b,g,r]).transpose()
-    return opencv_image
-
-def crop_center(img,cropx,cropy):
-    h, w = img.shape[:2]
-    startx = w//2-(cropx//2)
-    starty = h//2-(cropy//2)
-    return img[starty:starty+cropy, startx:startx+cropx]
-
-def resize_down_to_1600_max_dim(image):
-    h, w = image.shape[:2]
-    if (h < 1600 and w < 1600):
-        return image
-
-    new_size = (1600 * w // h, 1600) if (h > w) else (1600, 1600 * h // w)
-    return cv2.resize(image, new_size, interpolation = cv2.INTER_LINEAR)
-
-def resize_to_256_square(image):
-    h, w = image.shape[:2]
-    return cv2.resize(image, (256, 256), interpolation = cv2.INTER_LINEAR)
-
-def update_orientation(image):
-    exif_orientation_tag = 0x0112
-    if hasattr(image, '_getexif'):
-        exif = image._getexif()
-        if (exif != None and exif_orientation_tag in exif):
-            orientation = exif.get(exif_orientation_tag, 1)
-            # orientation is 1 based, shift to zero based and flip/transpose based on 0-based values
-            orientation -= 1
-            if orientation >= 4:
-                image = image.transpose(Image.TRANSPOSE)
-            if orientation == 2 or orientation == 3 or orientation == 6 or orientation == 7:
-                image = image.transpose(Image.FLIP_TOP_BOTTOM)
-            if orientation == 1 or orientation == 2 or orientation == 5 or orientation == 6:
-                image = image.transpose(Image.FLIP_LEFT_RIGHT)
-    return image
-
 def main(argv):
-    #(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
-    # Import the TF graph
-    for arg in sys.argv[1:]:
-        print(arg)
-        
     graph = load_pb(argv[0])
 
     labels = []
-    # labels_filename = "labels.txt"
-    
-    # print('out name is ', graph.as_graph_def().node[-1].name)
-    # for node in graph.as_graph_def().node:
-    #     print(node.op, node.name)
+    labels_filename = argv[2]
+
+    # Create a list of labels.
+    with open(labels_filename, 'rt') as lf:
+        for l in lf:
+            labels.append(l.strip())
    
-    # Load from a file
     imageFile = argv[1]
-    image = Image.open(imageFile)
+    image = cv2.imread(imageFile)
 
-    # Update orientation based on EXIF tags, if the file has orientation info.
-    image = convert_to_opencv(image) 
+    print(np.shape([image]))
 
-    cv2.imwrite("res.jpg", image)  
+    image_mean = 127.5
+    image_std = 127.5
+    image_size_x = 299
+    image_size_y = 299 
 
-    image = resize_down_to_1600_max_dim(image)  
+    input_image = cv2.resize(image, (image_size_x, image_size_y), interpolation = cv2.INTER_LINEAR)
+    # print([input_image])
+    print(np.shape([input_image]))
 
-    h, w = image.shape[:2]
-    min_dim = min(w,h)
-    max_square_image = crop_center(image, min_dim, min_dim)
+    # img = [[[[0 for i in range(1)] for x in range(image_size_x)] for y in range(image_size_y)]for z in range(3)]
 
-    augmented_image = resize_to_256_square(max_square_image)
+    img = np.zeros((1,299,299,3))
+    
+    for i in range(image_size_x):
+        for j in range(image_size_y):
+            pixelValue = input_image[i][j]
+            # print()
+            # print(pixelValue)
+            # print((pixelValue - image_mean)/ image_std)
+            img[0, i, j] = (pixelValue - image_mean) / image_std
 
-    # with tf.compat.v1.Session() as sess:
-    #     input_tensor_shape = sess.graph.get_tensor_by_name('input:0').shape.as_list()
-    # network_input_size = input_tensor_shape[1]
+    img = np.array(img, dtype=float)
+    print(np.shape(img))
 
-    # Crop the center for the specified network_input_Size
-    # augmented_image = crop_center(augmented_image, network_input_size, network_input_size)
+    print(img.min())
+    print(img.max())
 
-    # # These names are part of the model and cannot be changed.
+    cv2.imwrite("res.jpg", input_image)  
+
     output_layer = 'output:0'
     input_node = 'input:0'
 
     with tf.compat.v1.Session() as sess:
+        sess.graph.as_default()
+        tf.import_graph_def(graph.as_graph_def(), name = "")
         try:
             prob_tensor = sess.graph.get_tensor_by_name(output_layer)
-            # predictions, = sess.run(prob_tensor, {input_node: [augmented_image] })
+            predictions, = sess.run(prob_tensor, {input_node: img})
         except KeyError:
             print ("Couldn't find classification output layer: " + output_layer + ".")
             print ("Verify this a model exported from an Object Detection project.")
             exit(-1)
 
-    # # Print the highest probability label
-    # highest_probability_index = np.argmax(predictions)
-    # print('Classified as: ' + labels[highest_probability_index])
-    # print()
+    # Print the highest probability label
+    highest_probability_index = np.argmax(predictions)
+    print()
+    print(highest_probability_index)
+    print('Classified as: ' + labels[highest_probability_index])
+    print()
 
 if __name__ == "__main__":
     main(sys.argv[1:])