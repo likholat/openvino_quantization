d3eb91cccfbbb15192e03b6fd2133f5b6191c27f
Anna Likholat <anna.likholat@intel.com>
2020-04-23 15:41:03 +0300

    right result but wrong label

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # openvino_quantization
+
+Link to download ResNet_V2_101 model:
+https://www.tensorflow.org/lite/guide/hosted_models
--- demo_classification_tf.py
+++ demo_classification_tf.py
@@ -2,6 +2,7 @@ import sys
 import tensorflow as tf
 import numpy as np
 import cv2
+import argparse
 
 def load_pb(path_to_pb):
     with tf.io.gfile.GFile(path_to_pb, "rb") as f:
@@ -11,50 +12,36 @@ def load_pb(path_to_pb):
         tf.import_graph_def(graph_def, name='')
         return graph
 
-def main(argv):
-    graph = load_pb(argv[0])
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("graph", help=".pb graph path")
+    parser.add_argument("image", help="image path")
+    parser.add_argument("labels", help=".txt labels path")
+    argv = parser.parse_args()
+
+    graph = load_pb(argv.graph)
 
     labels = []
-    labels_filename = argv[2]
+    labels_filename = argv.labels
 
     # Create a list of labels.
     with open(labels_filename, 'rt') as lf:
         for l in lf:
             labels.append(l.strip())
-   
-    imageFile = argv[1]
-    image = cv2.imread(imageFile)
 
-    print(np.shape([image]))
+    imageFile = argv.image
+    image = cv2.imread(imageFile)
 
     image_mean = 127.5
     image_std = 127.5
     image_size_x = 299
     image_size_y = 299 
 
-    input_image = cv2.resize(image, (image_size_x, image_size_y), interpolation = cv2.INTER_LINEAR)
-    # print([input_image])
-    print(np.shape([input_image]))
-
-    # img = [[[[0 for i in range(1)] for x in range(image_size_x)] for y in range(image_size_y)]for z in range(3)]
-
-    img = np.zeros((1,299,299,3))
-    
-    for i in range(image_size_x):
-        for j in range(image_size_y):
-            pixelValue = input_image[i][j]
-            # print()
-            # print(pixelValue)
-            # print((pixelValue - image_mean)/ image_std)
-            img[0, i, j] = (pixelValue - image_mean) / image_std
+    image = cv2.resize(image, (image_size_x, image_size_y), interpolation = cv2.INTER_LINEAR)
+    img = image.reshape((1, 299, 299, 3))
+    img = (img - image_mean) / image_std
 
     img = np.array(img, dtype=float)
-    print(np.shape(img))
-
-    print(img.min())
-    print(img.max())
-
-    cv2.imwrite("res.jpg", input_image)  
 
     output_layer = 'output:0'
     input_node = 'input:0'
@@ -78,4 +65,4 @@ def main(argv):
     print()
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
