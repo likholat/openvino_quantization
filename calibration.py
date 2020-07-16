6d1a0c226de878b402925f7724f0fc751c553a85
Anna Likholat <anna.likholat@intel.com>
2020-07-16 19:34:53 +0300

    Develop (#3)
    * first commit
    
    * wrong result
    
    * right result but wrong label
    
    * right result but wrong label
    
    * right result but wrong label
    
    * wrong label
    
    * probability in range [0,1]
    
    * fix comments
    
    * fix comments
    
    * fix comment
    
    * fix comment
    
    * fix commets
    
    * fix comments
    
    * first commit
    
    * classify added
    
    * rename file
    
    * classification constructor added
    
    * to save changes
    
    * fix comments
    
    * fix comment
    
    * fit comments
    
    * fixes
    
    * fix comments
    
    * fix comments
    
    * fix comments
    
    * fix comments
    
    * fix comments
    
    * fix coments
    
    * fix coments
    
    * fix comments
    
    * fix comments
    
    * fixes
    
    * fixes
    
    * fixes
    
    * fixes
    
    * first commit
    
    evaluate added
    
    fixes
    
    fixes
    
    fixes
    
    fixes
    
    fixes
    
    changed README
    
    * fixed comments
    
    * quantisation added
    
    * fixed metric
    
    * fixed comments
    
    * fixed comments
    
    * fixed comments
    
    * estimated accuracy added
    
    * fixed comments
    
    * fixes
    
    * fixes
    
    * fixes
    
    * fixes
    
    * fixes
    
    * fixes
    
    * fixes

---
+++ calibration.py
@@ -0,0 +1,149 @@
+import os
+import cv2 as cv
+import numpy as np
+import argparse
+
+from addict import Dict
+from compression.graph import load_model, save_model
+from compression.data_loaders.data_loader import DataLoader
+from compression.engines.ie_engine import IEEngine
+from compression.custom.metric import Metric
+from compression.pipeline.initializer import create_pipeline
+
+parser = argparse.ArgumentParser(description="Quantizes OpenVino model to int8.",
+                                    add_help=True, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument("--xml", default="resnet_v2_101_299_opt.xml", help="XML file for OpenVINO to quantize")
+parser.add_argument("--model_name", default="resnet_v2_101_299_opt", help="OpenVINO model name")
+parser.add_argument("--annotation", default="val.txt", help="Manifest file (txt file with filenames of images and labels)")
+parser.add_argument("--data", default="ILSVRC2012_img_val", help="Data directory root")
+parser.add_argument("--int8_dir", default="./model/optimized", help="INT8 directory for calibrated OpenVINO model")
+
+argv = parser.parse_args()
+
+class DatasetsDataLoader(DataLoader):
+ 
+    def __init__(self, config):
+        if not isinstance(config, Dict):
+            config = Dict(config)
+        super().__init__(config)
+
+        imagesIds = {}
+
+        with open(config['annotation_file'], 'rt') as f:
+             dataset = f.read().strip().split('\n') 
+
+        self.images = []
+        self.annotations = []
+
+        for i, value in enumerate(dataset):
+            img_path, label = value.rsplit(' ')
+            label = int(label) + 1
+
+            img_path = os.path.join(config['data_source'], img_path)
+
+            self.images.append(img_path)
+            self.annotations.append(label)
+
+        self.images = self.images[:1000]
+
+    @property
+    def size(self):
+        return len(self.images)
+
+    def __len__(self):
+        return self.size
+
+    def __getitem__(self, item):
+        img = cv.imread(self.images[item])
+
+        image_mean = 127.5
+        image_std = 127.5
+        image_size_x = 299
+        image_size_y = 299 
+
+        img = cv.resize(img, (image_size_x, image_size_y))
+        img = np.expand_dims(img, axis=0)
+        img = (img - image_mean) / image_std
+        img = img.astype(np.float32)
+        img = img.transpose((0, 3, 1, 2))
+
+        return (item, self.annotations[item]), img
+
+class AccuracyMetric(Metric):
+    def __init__(self):
+        super().__init__()
+        self.name = "Accuracy"
+        self._values = []
+
+    @property
+    def value(self):
+        """ Returns accuracy metric value for the last model output. """
+        return {self.name: [self._values[-1]]}
+
+    @property
+    def avg_value(self):
+        """ Returns accuracy metric value for all model outputs. """
+        print("{} = {}".format(self.name, sum(self._values)/len(self._values)))
+
+    def update(self, outputs, labels):
+        """ Updates prediction matches.
+        :param output: model output
+        :param target: annotations
+
+        Put your post-processing code here.
+        Put your custom metric code here.
+        The metric gets appended to the list of metric values
+        """
+        result = np.argsort(outputs[0][0])[::-1][:1]
+
+        self._values.append(1 if result[0] == labels[0] else 0)
+
+    def reset(self):
+        """ Resets collected matches """
+        self._values = []
+
+    @property
+    def higher_better(self):
+        """Attribute whether the metric should be increased"""
+        return True
+
+    def get_attributes(self):
+        return {}
+
+model_config = Dict({
+    'model_name': argv.model_name,
+    "model": argv.xml,
+    "weights": argv.xml.split('.xml')[0] + '.bin'
+})
+engine_config = {
+    'device': 'CPU',
+    'stat_requests_number': 4,
+    'eval_requests_number': 4
+}
+dataset_config = {
+    'data_source': argv.data,
+    'annotation_file': argv.annotation,
+}
+algorithms = [
+    {
+        'name': 'DefaultQuantization',
+        'params': {
+            'target_device': 'CPU',
+            'preset': 'performance',
+            'stat_subset_size': 300
+        }
+    }
+]
+
+model = load_model(model_config)
+
+data_loader = DatasetsDataLoader(dataset_config)
+metric = AccuracyMetric()
+
+loss = None
+engine = IEEngine(engine_config, data_loader, metric, loss)
+pipeline = create_pipeline(algorithms, engine)
+
+compressed_model = pipeline.run(model)
+save_model(compressed_model, argv.int8_dir)
---
+++ evaluate.py
@@ -0,0 +1,53 @@
+import tensorflow as tf
+import numpy as np
+import os
+import argparse
+import cv2 as cv
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--xml', help='.xml model path', default='resnet_v2_101_299_frozen.pb')
+    parser.add_argument('--dataset', help='database path', required=True)
+    parser.add_argument('--engine', help='target engine', required=True, choices=['tf', 'opvn'])
+    argv = parser.parse_args()
+
+    if argv.engine == 'tf':
+        from classification.classification_tf import TensorFlowClassification
+        network = TensorFlowClassification(argv.graph)
+    else:
+        from classification.classification_opvn import OpenVinoClassification
+        model_xml = os.path.join(argv.xml)
+        model_bin = model_xml.split('.xml')[0] + '.bin'
+
+        network = OpenVinoClassification(model_xml, model_bin)
+
+    with open(os.path.join(argv.dataset, 'val.txt'), 'rt') as f:
+        vals = f.read().strip().split('\n')
+
+    top_1 = 0
+    top_5 = 0
+
+    for i, value in enumerate(vals):
+        if(i % 1000 == 0):
+            print(i)
+
+        img_path, label = value.rsplit(' ')
+        label = int(label) + 1
+
+        imgPath = os.path.join(argv.dataset, img_path)
+        img = cv.imread(imgPath)
+        probability, results = network.classify(img, 5)
+
+        if label == results[0]:
+            top_1 += 1
+
+        if label in results:
+            top_5 += 1
+
+
+    print()
+    print('Top 1 accuracy: ' + str(top_1 / len(vals)))
+    print('Top 5 accuracy: ' + str(top_5 / len(vals)))
+
+if __name__ == "__main__":
+    main()
